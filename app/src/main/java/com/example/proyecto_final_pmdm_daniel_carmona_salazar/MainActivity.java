b6f7ea4b4d23ec8ee4f0e52ea71d89effb170832
dani <danirm1113@gmail.com>
2021-01-26 18:46:22 +0100

    Agrego los archivos de proyecto

---
+++ app/src/main/java/com/example/proyecto_final_pmdm_daniel_carmona_salazar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_final_pmdm_daniel_carmona_salazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
