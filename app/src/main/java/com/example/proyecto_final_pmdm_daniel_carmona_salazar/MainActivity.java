df1cbf3bb56759485b3efdb3b9ad3ac91d4666f9
Dani <danirm1113rgmail.com>
2021-02-18 00:31:15 +0100

    Sesión 18/02/2021 - MainActivity completado.

--- app/src/main/java/com/example/proyecto_final_pmdm_daniel_carmona_salazar/MainActivity.java
+++ app/src/main/java/com/example/proyecto_final_pmdm_daniel_carmona_salazar/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proyecto_final_pmdm_daniel_carmona_salazar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void empezarAplicación(View view) {
+
     }
 }
