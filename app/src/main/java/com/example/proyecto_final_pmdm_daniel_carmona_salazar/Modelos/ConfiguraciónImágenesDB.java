656c9acca4a518a7a1b674ca6e29e4bbbfcc917e
dani <danirm1113@gmail.com>
2021-02-17 19:19:51 +0100

    Sesión 2 17 de febrero. 2h
    - Cambios en ActivityMain

--- app/src/main/java/com/example/proyecto_final_pmdm_daniel_carmona_salazar/Modelos/ConfiguraciónImágenesDB.java
+++ app/src/main/java/com/example/proyecto_final_pmdm_daniel_carmona_salazar/Modelos/ConfiguraciónImágenesDB.java
@@ -1,6 +1,6 @@
 package com.example.proyecto_final_pmdm_daniel_carmona_salazar.Modelos;
 
 public class ConfiguraciónImágenesDB {
-    public static final int ANCHO_FOTO = 200;
-    public static final int ALTO_FOTO = 100;
+    public static final int ANCHO_FOTO = 100;
+    public static final int ALTO_FOTO = 200;
 }
