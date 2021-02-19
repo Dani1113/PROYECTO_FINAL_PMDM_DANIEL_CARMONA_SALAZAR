dd002cfecf9b455fdad8b63973e356d007dfab96
dani <danirm1113@gmail.com>
2021-02-19 17:54:55 +0100

    Sesión 2 17 de febrero. 2h
    - Cambios en fichero Configuración

--- app/src/main/java/com/example/proyecto_final_pmdm_daniel_carmona_salazar/Modelos/ConfiguraciónDB.java
+++ app/src/main/java/com/example/proyecto_final_pmdm_daniel_carmona_salazar/Modelos/ConfiguraciónDB.java
@@ -4,7 +4,7 @@ public class ConfiguraciónDB {
     public static final String HOSTDB = "10.0.2.2";
     public static final String NOMBREDB = "tienda_videojuegos_android";
     public static final String USUARIODB = "root";
-    public static final String CLAVEDB = "Legendario11";
+    public static final String CLAVEDB = "1234";
     private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     public static final String PUERTOMYSQL = "3306";
     public static final String URLMYSQL = "jdbc:mysql://"+ HOSTDB + ":" + PUERTOMYSQL+"/" + NOMBREDB + OPCIONES;
